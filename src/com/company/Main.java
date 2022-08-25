13882e50a61af8dff317e7a8f97227391eba84cf
jaasonyoung222 <duceyoung8@gmail.com>
2022-08-25 17:25:08 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+import javax.swing.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String Moe = JOptionPane.showInputDialog("Name");
+        System.out.println("You are the king of rock & roll");
+//System.out.println("You are not the king.");
+
+
+    }
+}
